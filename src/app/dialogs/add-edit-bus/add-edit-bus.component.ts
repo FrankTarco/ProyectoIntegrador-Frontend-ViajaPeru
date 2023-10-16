f18fbcd875da204b2fc4a2d23b8832256e46a0c0
Luismi0612 <lgutierrezolivos@gmail.com>
2023-10-16 11:57:20 -0500

    Validaciones

--- src/app/dialogs/add-edit-bus/add-edit-bus.component.ts
+++ src/app/dialogs/add-edit-bus/add-edit-bus.component.ts
@@ -14,6 +14,9 @@ import Swal from 'sweetalert2';
 })
 export class AddEditBusComponent implements OnInit{
 
+  estado ='form'; 
+  letraspatt = /^[a-zA-ZáéíóúñüÁÉÍÓÚÑÜ\s]{2,20}$/; 
+  codigoPattern = /^[A-Z0-9Ñ]{3}-[A-Z0-9Ñ]{3}$/;
 lstCombustible:Combustible[] = []
 lstServicio:Servicio[] = []
 
@@ -70,7 +73,150 @@ agregarGuion(){
   }
 }
 
+validarCampos(): boolean {
+  const añoActual = new Date().getFullYear();
+  //console.log(añoActual);
+
+  if (
+    !this.objBus.placa ||
+    !this.objBus.marca ||
+    this.objBus.total_asientos == 0 ||
+    this.objBus.total_pasajeros == 0 ||
+    this.objBus.fabricacion == 0 ||
+    this.objBus.cantidad_ruedas == 0 ||
+    this.objBus.estado === -1
+  ) {
+    
+    
+    return false;
+  }
+
+  
+  if (!this.codigoPattern.test(this.objBus.placa)) {
+   
+    this.estado='placa';
+    console.log(this.estado);
+    return false;
+  }else if (!this.letraspatt.test(this.objBus.marca)){
+    this.estado='marca';
+    return false;
+  }else if (this.objBus.total_asientos! < 50 || this.objBus.total_asientos! > 100 ){
+    this.estado='asiento';
+  
+    return false;
+  }else if (this.objBus.total_pasajeros! > this.objBus.total_asientos! ){
+    this.estado='maspasajero';
+    
+    return false;
+  }else if(this.objBus.total_pasajeros! < 50 || this.objBus.total_pasajeros! > 100){
+    this.estado='intervalopasajeros';
+   
+    return false;
+  }else if(this.objBus.fabricacion! < 2010 || this.objBus.fabricacion! > añoActual){
+    this.estado='fabricacion';
+    
+    return false;
+  }else if(this.objBus.cantidad_ruedas! % 2 !== 0){
+    this.estado='impar';
+   
+    return false;
+  }else if(this.objBus.cantidad_ruedas! < 4 || this.objBus.cantidad_ruedas! > 16){
+    this.estado='ruedas';
+    return false;
+  }
+  this.estado='form'
+  return true;
+
+}
+
+
+
+
+
+
 registrar(){
+  console.log(this.estado);
+
+  if (this.validarCampos()==false && this.estado=='form') {
+    Swal.fire({
+      icon: 'error',
+      title: 'Ingrese Datos',
+      text: "Todos los campos son obligatorios para el Registro",
+    })
+    
+
+  }else if (this.validarCampos()==false && this.estado=='placa') {
+    Swal.fire({
+      icon: 'error',
+      title: 'Ingrese Placa Correcta',
+      text: "Ejmeplo de placa correcta : ABC-123",
+    })
+    
+  }else if (this.validarCampos()==false && this.estado=='marca') {
+    Swal.fire({
+      icon: 'error',
+      title: 'Ingrese Marca Correcta',
+      text: "Para el campo marca solo caracteres alfabeticos",
+    })
+    
+  }else if (this.validarCampos()==false && this.estado=='asiento') {
+    Swal.fire({
+      icon: 'error',
+      title: 'Cantidad de Asientos Incorrecta',
+      text: "El intervalo permitido es de 50 a 100 asientos por bus",
+    })
+   
+  }else if (this.validarCampos()==false && this.estado=='maspasajero') {
+    Swal.fire({
+      icon: 'error',
+      title: 'Cantidad de Pasajeros superior a los asientos',
+      text: "La cantidad de pasajeros no debe ser mayor a la capacidad del Bus",
+    })
+    
+  }else if (this.validarCampos()==false && this.estado=='intervalopasajeros') {
+    Swal.fire({
+      icon: 'error',
+      title: 'Cantidad de Pasajeros correcta',
+      text: "El intervalo permitido es de 50 a 100 pasajeros por bus",
+    })
+   
+  }else if (this.validarCampos()==false && this.estado=='fabricacion') {
+    Swal.fire({
+      icon: 'error',
+      title: 'Año de Frabricacion Incorrecta',
+      text: "El año de fabricacion no debe ser menor a 2010 o mayor a 2023",
+    })
+    
+  }else if (this.validarCampos()==false && this.estado=='impar') {
+    Swal.fire({
+      icon: 'error',
+      title: 'Cantidad de Ruedas Incorrecta',
+      text: "La cantidad de ruedas no debe ser un numero impar",
+    })
+    
+  }else if (this.validarCampos()==false && this.estado=='ruedas') {
+    Swal.fire({
+      icon: 'error',
+      title: 'Cantidad de Ruedas Incorrecta',
+      text: "La cantidad debe ser mayor a 4 y menor a 16",
+    })
+    
+  } else if (this.objBus.cod_combustible! == -1) {
+
+    Swal.fire({ icon: 'info', title: 'Seleccione el tipo de Combustible' })
+    this.objBus.cod_combustible = -1;
+    
+
+  } else if (this.objBus.cod_servicio! == -1) {
+    Swal.fire({ icon: 'info', title: 'Seleccione el tipo de Servicio' })
+    this.objBus.cod_servicio = -1;
+    
+  }
+  
+  
+  
+  else if (this.validarCampos()==true){
+
 
   if(this.data){
     this.busService.actualizarBus(this.objBus).subscribe(
@@ -89,6 +235,9 @@ registrar(){
       }
     )
   }
+  }
+
+
 }
 
 }
